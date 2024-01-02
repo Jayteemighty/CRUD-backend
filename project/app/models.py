7bc3158b5bab79b4f8ed532816ffb9c49a237b33
Adesina Joshua <tolujosh1@gmail.com>
2024-01-02 08:39:52 +0100

    login configuration

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    username = models.CharField(max_length=100)
+    description = models.CharField(max_length=100000)
+    created_at = models.DateTimeField(auto_now_add=True)
--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/login')
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
