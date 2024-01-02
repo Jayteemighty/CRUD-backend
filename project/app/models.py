9c21152e834b6947a221a06531a0e2dba4d6bdaf
Adesina Joshua <tolujosh1@gmail.com>
2024-01-02 18:51:11 +0100

    authentication done

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class post(models.Model):
-    username = models.CharField(max_length=100)
-    description = models.CharField(max_length=100000)
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+        return self.title + "\n" + self.description
--- project/project/settings.py
+++ project/project/settings.py
@@ -121,9 +121,10 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL = '/login
+LOGOUT_REDIRECT_URL = '/login'
